b88d36f0cfd2073e1687b6dce5ee8a14f28166a1
riyas <riyasksalim@hotmail.com>
2017-04-28 22:40:11 -0700

    Added Faceboo scopes

--- App10/Adapter/FacebookUser.cs
+++ App10/Adapter/FacebookUser.cs
@@ -25,51 +25,58 @@ namespace App10.Adapter
         public string os { get; set; }
     }
 
-    public class School
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-    }
-
-    public class With
-    {
-        public string name { get; set; }
-        public string id { get; set; }
-    }
-
-    public class Education
-    {
-        public School school { get; set; }
-        public string type { get; set; }
-        public string id { get; set; }
-        public List<With> with { get; set; }
-    }
-
     public class FavoriteAthlete
     {
         public string id { get; set; }
         public string name { get; set; }
     }
 
-    public class Hometown
+    public class Datum
     {
-        public string id { get; set; }
         public string name { get; set; }
+        public string id { get; set; }
+        public string created_time { get; set; }
+    }
+
+    public class Cursors
+    {
+        public string before { get; set; }
+        public string after { get; set; }
+    }
+
+    public class Paging
+    {
+        public Cursors cursors { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Likes
+    {
+        public List<Datum> data { get; set; }
+        public Paging paging { get; set; }
+    }
+
+    public class Summary
+    {
+        public int total_count { get; set; }
+    }
+
+    public class Friends
+    {
+        public List<object> data { get; set; }
+        public Summary summary { get; set; }
     }
 
     public class FaceBookUser
     {
         public string id { get; set; }
         public string name { get; set; }
-        public string birthday { get; set; }
         public Cover cover { get; set; }
         public List<Device> devices { get; set; }
-        public List<Education> education { get; set; }
-        public string email { get; set; }
         public string first_name { get; set; }
         public string gender { get; set; }
         public List<FavoriteAthlete> favorite_athletes { get; set; }
-        public List<string> interested_in { get; set; }
-        public Hometown hometown { get; set; }
+        public Likes likes { get; set; }
+        public Friends friends { get; set; }
     }
 }
--- App10/FacebookActivity.cs
+++ App10/FacebookActivity.cs
@@ -23,10 +23,52 @@ namespace App10
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.FacebookDetails);
             // Create your application here
-
+            var scopes= "email,user_hometown,user_religion_politics,publish_actions,user_likes"+
+"user_status"+
+"user_about_me" +
+"user_location" +
+"user_tagged_places" +
+"user_birthday" +
+"user_photos" +
+"user_videos" +
+"user_education_history" +
+"user_posts" +
+"user_website" +
+"user_friends" +
+"user_relationship_details" +
+"user_work_history" +
+"user_games_activity" +
+"user_relationships" +
+"Events, Groups&Pages" +
+"ads_management" +
+"pages_messaging" +
+"read_page_mailboxes" +
+"ads_read" +
+"pages_messaging_payments" +
+"rsvp_event" +
+"business_management" +
+"pages_messaging_phone_number" +
+"user_events" +
+"manage_pages" +
+"pages_messaging_subscriptions" +
+"user_managed_groups" +
+"pages_manage_cta" +
+"pages_show_list" +
+"pages_manage_instant_articles" +
+"publish_pages" +
+"user_actions.books" +
+"user_actions.music" +
+"user_actions.video" +
+"user_actions.fitness" +
+"user_actions.news" +
+"Other" +
+"read_audience_network_insights" +
+"read_custom_friendlists" +
+"read_insights";
+           
             var auth = new OAuth2Authenticator(
                     clientId: "168801230313991",
-                    scope: "",
+                    scope: "user_friends,user_likes",
                     authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
                     redirectUrl: new Uri("http://www.facebook.com/connect/login_success.html"));
 
@@ -41,7 +83,7 @@ namespace App10
             {
                 try
                 {
-                    var request = new OAuth2Request("GET", new System.Uri("https://graph.facebook.com/me?fields=id,name,birthday,about,cover,devices,education,email,first_name,gender,favorite_athletes,favorite_teams,inspirational_people,interested_in,hometown"), null, e.Account);
+                    var request = new OAuth2Request("GET", new System.Uri("https://graph.facebook.com/me?fields=id,name,birthday,about,cover,devices,education,email,first_name,gender,favorite_athletes,favorite_teams,inspirational_people,interested_in,hometown,likes,friends,tagged_places"), null, e.Account);
                     var FbResponse = await request.GetResponseAsync();
                     var json = FbResponse.GetResponseText();
 
